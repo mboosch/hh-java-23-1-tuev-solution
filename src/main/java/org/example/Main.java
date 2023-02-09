ee28432d6ffbc89fc31c0a500c8fc092faf9d884
davidskottke <david.skottke@neuefische.de>
2023-02-09 16:36:13 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car validCar = new Car(4, 5, true, true);
+        Car invalidCar = new Car(3, 4, true, true);
+        CarInspektionService carInspektionService = new CarInspektionService();
+        System.out.println(carInspektionService.checkCar(validCar));
+        System.out.println(carInspektionService.checkCar(invalidCar));
     }
 }
